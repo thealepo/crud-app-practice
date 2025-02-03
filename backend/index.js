370144286de14687a33914df77be48a8febfbaea
thealepo <sanchezc.alexe@gmail.com>
2025-02-03 09:49:41 -0500

    Problems accessing localhost

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,20 @@
 import express from "express"
+import mysql from "mysql"
 
+const express = require("express");
 const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Aleposan2730$anchZ",
+    database: "test"
+})
+
+app.get("/", (req, res) => {
+    res.json("Hello this is the backend");
+});
+
+app.listen(8800, () => {
+    console.log("Connected to backend!");
+});
