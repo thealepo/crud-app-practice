ef84a4dc4e5e321bde2c4764db67470e8fe8e8da
thealepo <sanchezc.alexe@gmail.com>
2025-02-03 09:57:10 -0500

    Corrected localhost error, but now facing MySQL error.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express"
 import mysql from "mysql"
 
-const express = require("express");
 const app = express();
 
 const db = mysql.createConnection({
@@ -14,6 +13,14 @@ const db = mysql.createConnection({
 app.get("/", (req, res) => {
     res.json("Hello this is the backend");
 });
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
 
 app.listen(8800, () => {
     console.log("Connected to backend!");
